0b25c8fcee535dd6e25ee8b2e18f171b37f1ce57
Sekhar <khanalsekhar13@gmail.com>
2024-08-08 16:46:39 +0545

    Add VPC module: Create VPC and public subnet

---
+++ modules/vpc/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.main.id
+}
